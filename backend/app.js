a650c575771729d05a2feb79c35dcc5dbdd09452
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-04-08 10:52:30 -0600

    Update app.js
    added more comments

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,27 @@
 "use strict";
-
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const aiRouter = require("./routes/ai"); // Import AI-related routes
+ 
+const express = require("express"); //express framework for backend set up
+const cors = require("cors"); //middleware to allow the frontend to communicate with the backend from different domains
+const morgan = require("morgan"); //logging library that logs incoming requests in a concise format for debugging
+const aiRouter = require("./routes/ai"); // Import AI-related routes to handel API requests related to AI
 
 const { NotFoundError } = require("./expressError");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
-//app.use(authenticateJWT);
+app.use(express.json()); //middleware that ensures that incoming requests with JSON payloads are parsed and available in req.body
+app.use(morgan("tiny")); //logs HTTP requests to the console for easy debugging
 
-// Use the AI routes
+// Use aiRouter for  handling requests to paths starting with /ai-endpoint
 app.use("/ai-endpoint", aiRouter); // Add the AI-related routes with a base path
 
-/** Handle 404 errors -- this matches everything */
+// middleware to catch any requests that don't match any defined routes, triggering NotFoundError. Will send a 404 response to the client.
 app.use(function (req, res, next) {
   return next(new NotFoundError());
 });
 
-/** Generic error handler; anything unhandled goes here. */
+// Generic error handler for any undandled errors. Checks if it is a non-test environment, logs the error stack, and returns a JSON response with an appropriate error status (500)
 app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
