c156176b204984173d7cc884ec72e535283bc405
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-04-08 10:44:29 -0600

    Update Express.js
    added more comments

--- backend/Express.js
+++ backend/Express.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const app = require('./app'); // Import the configured Express app
 const { PORT } = require('./config'); // Import the port from config.js
 
-// CORS configuration
+// CORS configuration - allow requests from render. 
 const corsOptions = {
   origin: 'https://tracker-front.onrender.com',
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200 // to avoid compatibility issues with older browsers
 };
 app.use(cors(corsOptions));
 
-// Use the imported PORT value to start the server
-app.listen(PORT, () => {
+// Use the imported PORT value to start the server and make it listen for incoming requests on the specified port. 
+app.listen(PORT, () => { //PORT value is coming from config.js for easier configuration for different environments
   console.log(`Server is running on port ${PORT}`);
 });
