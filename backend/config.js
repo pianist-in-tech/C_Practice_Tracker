b083ce6556e9afddcb40b2e80db8a7fbe1911fa2
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-04-08 10:58:42 -0600

    Update config.js
    added more comments

--- backend/config.js
+++ backend/config.js
@@ -1,24 +1,23 @@
 "use strict";
 
-require("dotenv").config();
+require("dotenv").config(); //loads environment variables from a .env file into process.env
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "development-key";
 
-const PORT = +process.env.PORT || 3001;
+const PORT = +process.env.PORT || 3001; //sets the port for the server to run on. Checks if process.env.PORT is set and falls back to 3001 in local environment.  
 
-// Use dev database, testing database, or via env var, production database
+// The logic below is for database set up (stretch goal). Determine the URI for the database connection based on the environment
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "postgresql:///practice_tracker_test"
       : process.env.DATABASE_URL || "postgresql:///practice_tracker";
 }
 
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
+//Bcrypt hashing algorithm used to securely store passwords. 1 for test, 12 for production. 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
+//below is for debugging purposes. 
 console.log("practice_tracker Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
